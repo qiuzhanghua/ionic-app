8cffc886cf6264115d5aeb7f3d4ab4c96f3bb061
qiuzhanghua <qiuzhanghua@icloud.com>
2024-04-17 16:34:57 +0800

    initial

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { IonApp, IonRouterOutlet } from '@ionic/vue';
+</script>
+
+<template>
+  <ion-app>
+    <ion-router-outlet />
+  </ion-app>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/router/index.ts
@@ -0,0 +1,26 @@
+import { createRouter, createWebHistory } from '@ionic/vue-router';
+import {RouteRecordRaw} from "vue-router";
+
+const routes: Array<RouteRecordRaw> = [
+    {
+        path: '/',
+        redirect: '/home',
+    },
+    {
+        path: '/home',
+        name: 'Home',
+        component: () => import('../view/Home.vue'),
+    },
+    {
+        path: '/about',
+        name: 'About',
+        component: import('../view/About.vue'),
+    },
+];
+
+const router = createRouter({
+    history: createWebHistory(process.env.BASE_URL),
+    routes
+});
+
+export default router;
